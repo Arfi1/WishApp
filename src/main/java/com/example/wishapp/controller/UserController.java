53246fe33b02a714257174e52be457d4bb5e0a33
arfi <arma0001@stud.kea.dk>
2024-04-11 16:53:51 +0200

    hh

--- src/main/java/com/example/wishapp/controller/UserController.java
+++ src/main/java/com/example/wishapp/controller/UserController.java
@@ -1,4 +1,19 @@
 package com.example.wishapp.controller;
 
+import com.example.wishapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class UserController {
+    @Autowired
+    UserService userService;
+
+    @GetMapping("/")
+    public String index(Model model) {
+        model.addAttribute("username", userService.ge)
+
+    }
 }
--- src/main/java/com/example/wishapp/repository/UserRepository.java
+++ src/main/java/com/example/wishapp/repository/UserRepository.java
@@ -1,4 +1,18 @@
 package com.example.wishapp.repository;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepository {
+
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+
+    public void insert(int id, String email, String name) {
+        String query = "INSERT INTO movie(userid, email, username)" + "VALUES (?, ?, ?);";
+        jdbcTemplate.update(query, id, email, name);
+    }
 }
