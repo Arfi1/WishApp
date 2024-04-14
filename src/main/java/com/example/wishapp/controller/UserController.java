be8ab1d0299bbaf5e73bc669d773be8b271c50a6
arfi <arma0001@stud.kea.dk>
2024-04-14 21:27:09 +0200

    init

--- src/main/java/com/example/wishapp/controller/UserController.java
+++ src/main/java/com/example/wishapp/controller/UserController.java
@@ -8,12 +8,14 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
+
     @Autowired
     UserService userService;
 
     @GetMapping("/")
     public String index(Model model) {
-        model.addAttribute("username", userService.ge)
-
+        return "home/index";
     }
+
+
 }
