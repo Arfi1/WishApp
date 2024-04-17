e84a4ed96ea7d442eaac642fb22510317a6d40b7
Emre <Emre2880@outlook.dk>
2024-04-17 16:11:14 +0200

    Branch

--- src/main/java/com/example/wishapp/controller/UserController.java
+++ src/main/java/com/example/wishapp/controller/UserController.java
@@ -5,10 +5,20 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class UserController {
-    @Autowired
-    UserService userService;
-
+    @PostMapping("/validate")
+    public String validate(Model model, @RequestParam String name,
+                           @RequestParam String password) {
+        if (name.equals(password)) {
+            model.addAttribute("message", "Right!");
+            return "home/Login";
+        } else {
+            model.addAttribute("message", "Wrong!");
+            return "home/Login";
+        }
+    }
 }
