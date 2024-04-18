2e307101ee30bacb3e2a98cbf172ed024d3e3ce2
DekaSkole <eser0004@stud.kea.dk>
2024-04-18 14:42:33 +0200

    new juice

--- src/main/java/com/example/wishapp/repository/WishListRepository.java
+++ src/main/java/com/example/wishapp/repository/WishListRepository.java
@@ -1,4 +1,30 @@
 package com.example.wishapp.repository;
 
+import com.example.wishapp.model.WishList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class WishListRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+
+    public WishList getWishList(int id){
+        String query = "SELECT * FROM wishlist WHERE id = ?;";
+        RowMapper<WishList> rowMapper = new BeanPropertyRowMapper<>(WishList.class);
+        return jdbcTemplate.queryForObject(query, rowMapper, id);
+    }
+    public void delete(int id) {
+        String query = "DELETE FROM wishlist WHERE id = ?";
+        jdbcTemplate.update(query, id);
+    }
+
+    public void insert(String title, String image, String wish){
+        String query = "INSERT INTO wishlist(title, image, wish" +
+                "VALUES (?, ?, ?);";
+        jdbcTemplate.update(query, title, image, wish);
+    }
 }
--- src/main/java/com/example/wishapp/repository/WishRepository.java
+++ src/main/java/com/example/wishapp/repository/WishRepository.java
@@ -1,4 +1,18 @@
 package com.example.wishapp.repository;
 
+import com.example.wishapp.model.Wish;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
 public class WishRepository {
+    @Autowired
+    JdbcTemplate jdbcTemplate;
+    public List<Wish> findAll() {
+        return null;
+    }
+
 }
