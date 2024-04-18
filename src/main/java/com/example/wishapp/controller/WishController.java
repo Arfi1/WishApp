37864b17eb29f4cce410bd81af82756a78003e13
DekaSkole <eser0004@stud.kea.dk>
2024-04-18 14:46:21 +0200

    new juice

--- src/main/java/com/example/wishapp/controller/WishController.java
+++ src/main/java/com/example/wishapp/controller/WishController.java
@@ -1,4 +1,40 @@
 package com.example.wishapp.controller;
 
+import com.example.wishapp.model.Wish;
+import com.example.wishapp.repository.WishRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.ui.Model;
+import java.util.List;
+
+@Controller
 public class WishController {
+
+    @Autowired
+    private WishRepository wishRepository;
+
+    @GetMapping("/")
+    public String showWishes(Model model) {
+        List<Wish> wishes = wishRepository.findAll();
+
+        model.addAttribute("wishes", wishes);
+        model.addAttribute("newWish", new Wish());
+        return "Wish";
+    }
+
+    @PostMapping("/wishes/add")
+    public String addWish(@ModelAttribute("newWish") Wish newWish) {
+      //  wishRepository.save(newWish);
+        return "redirect:/";
+    }
+
+    @GetMapping("/wishes/delete/{id}")
+    public String deleteWish(@PathVariable Long id) {
+        //wishRepository.deleteById(id);
+        return "redirect:/";
+    }
 }
--- src/main/java/com/example/wishapp/service/WishListService.java
+++ src/main/java/com/example/wishapp/service/WishListService.java
@@ -1,4 +1,17 @@
 package com.example.wishapp.service;
 
+import com.example.wishapp.model.WishList;
+import com.example.wishapp.repository.WishListRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
 public class WishListService {
+   @Autowired
+    private WishListRepository wishListRepository;
+
+    public WishList getWishListRepository(int id) {
+        return wishListRepository.getWishList(id);
+    }
 }
