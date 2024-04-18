adeaa9afdbd11dccd7794d4d8cde11341736eda3
jafar <jaal0002@stud.kea.dk>
2024-04-19 00:44:23 +0200

    test

--- src/main/java/com/example/wishapp/controller/WishController.java
+++ src/main/java/com/example/wishapp/controller/WishController.java
@@ -1,46 +1,53 @@
 package com.example.wishapp.controller;
 
 import com.example.wishapp.model.Wish;
-import com.example.wishapp.repository.WishRepository;
-import com.example.wishapp.service.WishListService;
 import com.example.wishapp.service.WishService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.ui.Model;
-import java.util.List;
 
 @Controller
 public class WishController {
+        @Autowired
+        private WishService wishService;
 
-    @Autowired
-    private WishService wishService;
+        @GetMapping("/confirm_delete")
+        public String confirmDelete(@RequestParam int id, Model model) {
+            model.addAttribute(wishService.getWish(id));
+            return "home/confirm_delete";
+        }
 
-    @GetMapping("/confirm_delete")
-    public String confirmDelete(@RequestParam int id, Model model) {
-        model.addAttribute(wishService.getWish(id));
-        return "home/confirm_delete";
-    }
-    @PostMapping("/delete")
-    public String delete(@RequestParam int id) {
-        wishService.deleteById(id);
-        return "redirect:/";
-    }
+        @PostMapping("/delete")
+        public String delete(@RequestParam int id) {
+            wishService.delete(id);
+            return "redirect:/";
+        }
 
-    @PostMapping("/wishes/add")
-    public String addWish(@ModelAttribute("newWish") Wish newWish) {
-        wishService.save(newWish);
-        return "redirect:/wishlist"; // Redirect to the wishlist page
-    }
-        @PostMapping("/validate")
-        public String validate(Model model, @RequestParam String name,
-                               @RequestParam String password) {
-            if (name.equals(password)) {
-                model.addAttribute("message", "Right!");
-                return "home/Login";
-            } else {
-                model.addAttribute("message", "Wrong!");
-                return "home/Login";
-            }
+        @GetMapping("/new")
+        public String insert() {
+            return "home/new";
+        }
+
+        @PostMapping("/insert")
+        public String insert(@RequestParam String title, @RequestParam int WishID,
+                             @RequestParam String beskrivelse, @RequestParam String billed, @RequestParam String URL) {
+            wishService.insert(WishID, beskrivelse, title, billed, URL);
+            return "redirect:/";
+
+        }
+
+        @GetMapping("/prepare_update")
+        public String prepareUpdate(@RequestParam int id, Model model) {
+            model.addAttribute(wishService.prepareUpdate(id));
+            return "home/update";
+        }
+
+        @PostMapping("/update")
+        public String update(@RequestParam String title, @RequestParam int WishID,
+                             @RequestParam String beskrivelse, @RequestParam String billed, @RequestParam String URL) {
+            wishService.update(WishID, beskrivelse, title, billed, URL);
+            return "redirect:/";
+
         }
     }
