b484e4cae41f6deb8f8a58f3013bcda0f517e988
Emre <Emre2880@outlook.dk>
2024-04-18 23:48:46 +0200

    Branch

--- src/main/java/com/example/wishapp/repository/UserRepository.java
+++ src/main/java/com/example/wishapp/repository/UserRepository.java
@@ -1,4 +1,14 @@
 package com.example.wishapp.repository;
 
+import com.example.wishapp.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class UserRepository {
+    @Autowired
+    UserService userService;
+    public void insert(int id, String email, String name) {
+        userService.insert(id, email, name);
+    }
 }
--- src/main/java/com/example/wishapp/repository/WishListRepository.java
+++ src/main/java/com/example/wishapp/repository/WishListRepository.java
@@ -1,4 +1,14 @@
 package com.example.wishapp.repository;
 
+import com.example.wishapp.model.WishList;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+@Repository
 public class WishListRepository {
+    @Autowired
+    WishListRepository wishListRepository;
+    public WishList getWishList(int id) {
+        return wishListRepository.getWishList(id);
+    }
 }
